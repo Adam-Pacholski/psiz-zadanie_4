d2901482f8a0b89ddd92fef8dff11d7b66ab9b82
Adam-Pacholski <adam.pacholski1985@gmail.com>
2022-04-11 22:25:47 +0200

    proby utworzenia plikow

--- psiz-zadanie_4/psiz-zadanie_4.cpp
+++ psiz-zadanie_4/psiz-zadanie_4.cpp
@@ -1,11 +1,55 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
+void createFile(const string nazwaPliku, const int licznik, const char wartosci);
+void plikLog(const string wartoscLoga);
+
 int main(int argc, char* argv[])
 {
-	cout <<  argc;
-	cout << "test wysy³ania na gita";
+	createFile("plik_1_0x55_100.bin", 100, 0x55);
+	createFile("plik_2_0x55_100.bin", 100, 0x55);
+	createFile("plik_3_0x55_100.bin", 100, 0x55);
+	createFile("plik_4_0x50_100.bin.bin", 100, 0x50);
+	plikLog("1");
+	plikLog("2");
 }
+
+void plikLog(const string wartoscLoga) {
+	const string log = "Log.txt";
+
+		ofstream plik;
+		plik.open(log, ios_base::app);
+		plik << wartoscLoga + "\n";
+		plik.close();
+}
+
+void createFile(const string nazwaPliku, const int licznik, const char wartosci)
+{
+	int iLicznik = 0;
+	ifstream mojPlik;
+	mojPlik.open(nazwaPliku);
+	
+	if (mojPlik)
+	{
+		cout << "Plik:" + nazwaPliku + " juz istnieje\n";
+	}
+	else {
+
+		ofstream plik(nazwaPliku);
+
+		for (size_t iLicznik = 0; iLicznik < licznik; iLicznik++)
+		{
+			plik << wartosci;
+		}
+	}
+
+	
+	
+	
+}
+
+
 
 
